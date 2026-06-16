1ac70dfd0d187644304e9697b96ca5c4bcf667ae
Strange <strange@localhost.localdomain>
2026-06-16 14:51:58 +0100

    Ajout de Bootstrap 5 pour un design moderne et responsive

--- Online_LMS/UploadAssignment.php
+++ Online_LMS/UploadAssignment.php
@@ -3,6 +3,8 @@
 <html lang="en">
 
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css"> rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -293,6 +295,7 @@
 
     <script src="bootstrap.bundle.js"></script>
     <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
